a0ebdb7843d559dab80124e872b965a578674dcb
Cmuss33 <connormusson@gmail.com>
2025-09-25 22:27:51 -0400

    initial ui

--- coaches-logging/src/App.jsx
+++ coaches-logging/src/App.jsx
@@ -1,33 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coachName, setCoachName] = useState("");
+  const [session, setSession] = useState("");
+  const [hours, setHours] = useState("");
+
+
+  const handleLog = () => {
+    // call backend
+
+    setCoachName("");
+    setSession("");
+    setHours("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='page-container'>
+        <div className='title-container'>
+          <span>Logging Hours</span>
+        </div>
+        <div className='coach-box'>
+          <div className='input-row1'>
+            <p className='input-header'>Coach Name</p>
+            <select value={coachName} onChange={(e) => setCoachName(e.target.value)} className='picker'>
+              <option value="">Select Coach</option>
+              <option value="Coach 1">Coach 1</option>
+              <option value="Coach 2">Coach 2</option>
+              <option value="Coach 3">Coach 3</option>
+            </select>
+          </div>
+
+          <div className='input-row2'>
+            <p className='input-header'>Session</p>
+            <select value={session} onChange={(e) => setSession(e.target.value)} className='picker'>
+              <option value="">Select Session</option>
+              <option value="Session 1">Session 1</option>
+              <option value="Session 2">Session 2</option>
+              <option value="Session 3">Session 3</option>
+            </select>
+          </div>
+
+          <div className='input-row2'>
+            <p className='input-header'>Number of Hours</p>
+            <input type='text' placeholder='Number of Hours' value={hours} onChange={(e) => setHours(e.target.value)} className='picker'></input>
+          </div>
+
+          <div className='button-container'>
+            <button className='set-button' onClick={handleLog}>Log Hours</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
